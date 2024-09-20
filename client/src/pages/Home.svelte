90a87592c67d894c69d11b69129031fc9cd891dc
Keagan <keygun2k1@gmail.com>
2024-09-20 12:28:11 +0200

    Init

---
+++ client/src/pages/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Counter from '../components/Counter.svelte';
+
+    export let params;
+</script>
+
+<Counter />
+<h1 class="text-3xl font-bold underline bg-primary-500">
+    Hello World!
+</h1>
